9bec08b13755122570affc6cd9a4ec737a0f77d4
Sagar Adhikari <sageradh@gmail.com>
2024-01-22 14:15:53 -0500

    Added derivation function

--- numeric_derivation.py
+++ numeric_derivation.py
@@ -1,3 +1,13 @@
 
 def derive(f, x, h=0.0001):
-    return None  # TODO: implement this function 
+    # Basic value tests
+    if f is None or x is None or h is None:
+        raise ValueError("Arguments 'f', 'x', and 'h' must not be None.")
+    if not isinstance(h, (int, float)):
+        raise ValueError("'h' must be a numeric value.")
+
+    # Making sure the function f() is callable
+    if not callable(f):
+        raise ValueError("'f' must be a callable function.")
+
+    return (f(x+h)-f(x))/h
