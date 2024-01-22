936bb91832546c6eed76d397604d015e12f3d6df
Sagar Adhikari <sageradh@gmail.com>
2024-01-22 14:16:35 -0500

    Added RMSE function

--- rmse.py
+++ rmse.py
@@ -3,5 +3,14 @@ import numpy as np
 def rmse(predictions, targets):
     pred = np.array(predictions)
     tar = np.array(targets)
-    rmse = None # TODO: Implement RMSE Calculation here...
+
+    # Implementation 1
+    mse = np.mean((pred - tar)**2)
+    print("mse1:\n", mse)
+
+    # Implementation 2
+    # mse = np.square(np.subtract(targets,predictions)).mean()
+    # print("mse2:\n", mse)
+    rmse = np.sqrt(mse)
+    
     return rmse
